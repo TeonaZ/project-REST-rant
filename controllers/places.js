643c69d395e5e20d155dc34443ade5862af27e0f
Teona <teonazaalishvili2@gmail.com>
2023-02-14 10:55:37 -0500

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,5 @@
 const router = require ('express').Router()
+const places = require('../models/places.js')
 
 router.get('/', (req, res)=> {
     console.log('controllers')
@@ -26,8 +27,20 @@ router.get('/new', (req, res) => {
 
 router.post('/', (req, res) => {
   console.log(req.body)
-  res.send('POST /places')
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
+
 
 
 
