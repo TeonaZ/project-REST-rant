4e923af7ddd2e7bc92134c77d0b5f154f4fa086f
Teona <teonazaalishvili2@gmail.com>
2023-02-14 21:09:30 -0500

    Remove code from places.js

--- controllers/places.js
+++ controllers/places.js
@@ -4,21 +4,9 @@ const places = require('../models/places.js')
 router.get('/', (req, res)=> {
     console.log('controllers')
     //res.render('places/index')
-let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/sandwich.jpg'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/cupOfCoffe.jpg'
-  }]
+
   
-    res.render('places/index', {places})
+  res.render('places/index', {places})
 })
 
 router.get('/new', (req, res) => {
--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 require('dotenv').config()
 const express = require('express')
+
 const app = express()
 
+//MIDDLEWARES
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
-
 app.use(express.urlencoded({ extended: true }))
 
-
+//Routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
