2feb63f591254be7720e27d636a10a7aaf8f1fc1
Teona <teonazaalishvili2@gmail.com>
2023-02-18 00:10:16 -0500

    Delete functionality impleted

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,7 +5,16 @@ function show (data) {
     return (
         <Def>
           <main>
+            <div id='infoPicture'>
+              <div>
+                <img src={data.place.pic} alt={data.place.name} width='600' height='600'/>
+              </div>
+            <div>
             <h1>{ data.place.name }</h1>
+            <h2>rating</h2>
+            <p>Not rated</p>
+            <h2>Description</h2>
+            <p>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</p>
             <a href="{`/places/${data.id}/edit`}" className="btn btn-warning"> 
                 Edit
             </a>  
@@ -13,8 +22,9 @@ function show (data) {
             <button type="submit" className="btn btn-danger">
                 Delete
             </button>
-            </form>     
-
+            </form>  
+            </div>   
+           </div>
           </main>
         </Def>
     )
