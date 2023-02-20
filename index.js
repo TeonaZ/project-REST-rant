07dd84e8c4d669fe55493b131f3cc38530d273a9
Teona <teonazaalishvili2@gmail.com>
2023-02-19 22:35:19 -0500

    MongoDB: connect

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const methodOverride = require('method-override')
+const mongoose = require('mongoose')
 
 const app = express()
 
@@ -22,6 +23,12 @@ app.get('/', (req, res) => {
 app.get('*', (req, res) => {
       res.render('error404')
       })
+
+// db connection
+mongoose.set('strictQuery', true)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error(err));
       
 
 app.listen(process.env.PORT, console.log(process.env.PORT))
