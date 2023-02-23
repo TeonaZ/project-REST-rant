ca7758c99a87c2cb8366c42b4e49f12f759d4217
Teona <teonazaalishvili2@gmail.com>
2023-02-22 21:41:08 -0500

    adding Seeding data, validations, and helper methods

---
+++ seeders/seed-places.js
@@ -0,0 +1,27 @@
+const places = require('../models/places')
+const db = require('../models')
+
+db.place.create([{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/sandwich.jpg',
+    founded: 1989
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/cupOfCoffe.jpg',
+    founded: 2020
+}])
+.then(() => {
+    console.log('Success!')
+    process.exit()
+})
+.catch(err => {
+    console.log('Failure!', err)
+    process.exit()
+})
+
